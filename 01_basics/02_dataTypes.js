ec69f4039a33e4639fff5fed0c21f93ffdb985b3
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-03-10 17:59:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using node.js, not browser
+
+/*
+types of data types:
+number => 2^53
+bigint high value
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol =>unique
+
+object 
+
+*/
+console.log(typeof undefined)
