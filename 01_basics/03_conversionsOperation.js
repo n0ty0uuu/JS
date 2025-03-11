15e7b345d03d1843291e39d97e9e2896872856c3
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-03-11 06:37:02 +0000

    operations on datatypes

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -1,9 +1,9 @@
 let score = 33
 
-console.log(typeof "Aditya") // you can get to know about the type of data present 
+//console.log(typeof "Aditya") // you can get to know about the type of data present 
 
 let valuseInNumber = Number(score)
-console.log(typeof valuseInNumber)
+// console.log(typeof valuseInNumber)
 
 // "33" => 33
 // "33abc" =>NaN
@@ -12,10 +12,33 @@ console.log(typeof valuseInNumber)
 let isLoggedIn = "aditya"
 
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanIsLoggedIn)
+// console.log(BooleanIsLoggedIn)
 
 /* 
 1 => true; o => false
 "" => false
 "Aditya" => true
 */
+
+
+// ********************* Operations ************************
+
+let value = 4
+let negValue = -value
+
+//console.log(negValue) => gives negative of the "value"
+
+// console.log(2**3) ==> this if 2^3 (2 ke power 3)
+
+let str1 = "Aditya"
+let str2 = " sakshi"
+
+let str3 = str1 + str2
+console.log(str1+str2)
+
+// let num1 =  "1" + 2 => op-12
+// let num2 =  1 + "2" => op-12
+// let num3 = "1" + 2 + 2 => op-122
+// let num4 =  1 + 2 + "2" => op-32
+
+// console.log(num4)
