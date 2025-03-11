04e53a26a3b71a69d017ab82166bd7a5ef7adcc6
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-03-11 05:18:26 +0000

    Conversions of datatypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+
+console.log(typeof "Aditya") // you can get to know about the type of data present 
+
+let valuseInNumber = Number(score)
+console.log(typeof valuseInNumber)
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = "aditya"
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+/* 
+1 => true; o => false
+"" => false
+"Aditya" => true
+*/
