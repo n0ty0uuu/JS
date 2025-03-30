187838ac0482b5130740cea30597b524d351e8aa
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-03-30 12:54:48 +0000

    about scope

---
+++ 03_basics/02_Scope.js
@@ -0,0 +1,15 @@
+
+var c = 500
+//{} -> Scope
+
+if(true){ // yaha sai leke 
+    // let a = 10
+// const b = 20
+var c = 30 // --> One should avoid using var in there codes
+}// yaha tak hai block scope bahar ka area hai global scope global scope kai bahar ke value scope kai ander available hote hai 
+// per scope kai ander ke value bahar ni jani chaiye 
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
