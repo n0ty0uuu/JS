cdf54c0bdfe5ddd27b8987e13a3b8286a6a229d5
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-04-05 12:09:48 +0000

    to be continued

---
+++ 03_basics/JsExecutionContext.js
@@ -0,0 +1,14 @@
+// JavaScript Execution Context
+// iska mtlb hota hai ke jo bhi file humne banai hai js kai ander usko js execute kaise krage run kaise krage 
+
+// ye Programs ko 2 phase mai run karti hai , jB run kre hai toh sabse pehle banta hai 
+//{} - > global execution context -> (this) iske ander GEC ko rakh dia jata hai
+// in browser (this) ke value aati hai 'Window Object' ye aati hai 
+
+// 2, Function execution context 
+
+
+// How program is Executed in Js
+
+
+
