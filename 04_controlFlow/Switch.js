35a2f5fb4067e5bc4874f1ab9bda4ab975044ed4
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-04-19 13:16:53 +0000

    ControlFlow statements

---
+++ 04_controlFlow/Switch.js
@@ -0,0 +1,26 @@
+//  switch(key) {}
+//  case value:
+
+//  break;
+
+//  default:
+//     break
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feburary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("Doesn't matched");
+        break;
+}
+// Agar break remove ho jai kisi conditon ka toh true condition kai neeche kai sarre wale execute ho jyange except "default"    
---
+++ 04_controlFlow/one.js
@@ -0,0 +1,30 @@
+// // if
+
+// // if(condition) { -> sabse pehle control flow statement 
+
+// // }
+
+// const isUserLoggedIN = true
+// if(isUserLoggedIN){
+//     console.log("Yess he's logged in");
+    
+// }// run cause conditon is true
+
+// const YourScore = 200
+
+// if(YourScore < 300){
+//     const MyOpenion = "Study more" 
+
+//     console.log(`Suggestion : ${MyOpenion}`);
+// }
+// //console.log(`Suggestion ${MyOpenion}`); -> won't run as it's out of scope
+
+
+// Shorthand notations
+
+const balance = 1000
+if(balance > 500) console.log("Ameer");// Implecit scope works in 1 line
+
+// else-if next hai aata hai vo skip kr rha 
+
+ 
