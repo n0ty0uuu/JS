dbdcd2cb2dc7ca009cbe535a0c39a6e4a5bccc91
n0ty0uuu <160602270+n0ty0uuu@users.noreply.github.com>
2025-04-20 10:24:57 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while 
+
+// let index = 0
+// while (index <=10) {
+//     console.log("Aditya");
+//     index++
+// }
+
+// let myArr = ['Doraemon','ben 10','Ash','shinchan']
+
+// let index = 0
+// while(index < myArr.length){
+//     console.log(`Value is ${myArr[index]}`);
+//     index++ 
+    
+// }
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
